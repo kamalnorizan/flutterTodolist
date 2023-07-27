21c80b6755b6b408a543f934a3061c6b027176b0
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-07-27 12:57:12 +0800

    inkwell

--- lib/screen/chartsScreen.dart
+++ lib/screen/chartsScreen.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:todolist/screen/home.dart';
 
 import '../widgets/mainDrawer.dart';
 
@@ -105,10 +106,15 @@ class ChartScreen extends StatelessWidget {
             Row(
               children: [
                 Expanded(
-                  child: StatCard(
-                      title: 'Jumlah Jualan',
-                      value: '20,000',
-                      bgColor: Colors.purpleAccent),
+                  child: InkWell(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Home()));
+                    },
+                    child: StatCard(
+                        title: 'Jumlah Jualan',
+                        value: '20,000',
+                        bgColor: Colors.purpleAccent),
+                  ),
                 ),
                 Expanded(
                     child: StatCard(
@@ -139,27 +145,32 @@ class ChartScreen extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: BarChart(
-                BarChartData(
-                    barTouchData: barTouchData,
-                    titlesData: titlesData,
-                    borderData: borderData,
-                    barGroups: barGroups,
-                    gridData: const FlGridData(
-                        drawVerticalLine: false, drawHorizontalLine: true),
-                    maxY: 20),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: BarChart(
+                    BarChartData(
+                        barTouchData: barTouchData,
+                        titlesData: titlesData,
+                        borderData: borderData,
+                        barGroups: barGroups,
+                        gridData: const FlGridData(
+                            drawVerticalLine: false, drawHorizontalLine: true),
+                        maxY: 20),
+                  ),
+                ),
               ),
             ),
-            Expanded(
-              child: BarChart(BarChartData(
-                  barTouchData: barTouchData,
-                  titlesData: titlesData,
-                  borderData: borderData,
-                  barGroups: barGroups,
-                  gridData: const FlGridData(
-                      drawVerticalLine: false, drawHorizontalLine: true),
-                  maxY: 20)),
-            ),
+            // Expanded(
+            //   child: BarChart(BarChartData(
+            //       barTouchData: barTouchData,
+            //       titlesData: titlesData,
+            //       borderData: borderData,
+            //       barGroups: barGroups,
+            //       gridData: const FlGridData(
+            //           drawVerticalLine: false, drawHorizontalLine: true),
+            //       maxY: 20)),
+            // ),
           ],
         ),
       ),
